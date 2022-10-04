6a07a96d16336a973677d002a1e4307ea8ff7f05
raven0613 <reevan0613@gmail.com>
2022-10-04 18:42:13 +0800

    layout: TweetCard

--- src/components/TweetCard.vue
+++ src/components/TweetCard.vue
@@ -1,30 +1,34 @@
 <template>
-    <div class="tweet__container">
+    <router-link to="#" class="tweet__container">
+
         <div class="tweet__avatar">
             <img class="tweet__avatar--photo" src="https://ipetgroup.com/photo/117457_0_620.jpeg" alt="">
         </div>
+
         <div class="tweet__info">
             
             <div class="tweet__top">
-                <span>Jane Cathy</span>
-                <span>@apple</span>
-                <span>- 3小時</span>
+                <span class="tweet__top--prim">Jane Cathy</span>
+                <span class="tweet__top--sec">@apple</span>
+                <span class="tweet__top--sec">- 3小時</span>
             </div>
             <div class="tweet__info--content">
                 Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. 
             </div>
             <div class="tweet__bottom">
-                <div class="tweet__bottom--icon">
+                <router-link to="/123" class="tweet__bottom--icon">
                     <img src="../assets/images/tweet_reply.svg" alt="">
-                </div>
-                <span>13</span>
-                <div class="tweet__bottom--icon">
+                    <span>13</span>
+                </router-link>
+                
+                <router-link to="/456" class="tweet__bottom--icon">
                     <img src="../assets/images/tweet_like.svg" alt="">
-                </div>
-                <span>76</span>
+                    <span>76</span>
+                </router-link>
+                
             </div>
         </div>
-    </div>
+    </router-link>
 </template>
 
 <script>
