e011abd5461c77579064535f8274f40c8db23db6
YohaLin <kate1995y@gmail.com>
2022-10-08 22:37:54 +0800

    modify: reslove conflict

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -1,5 +1,7 @@
 import { apiHelper } from "../utils/helpers";
-
+const getToken = () => {
+  localStorage.getItem('token')
+}
 
 
 export default {
@@ -11,21 +13,15 @@ export default {
       password
     })
   },
-
-const getToken = () => {
-    localStorage.getItem('token')
-}
-
-export default{
-    getAdminTweets () {
-        return apiHelper.get('/admin/tweets', {
-            headers: { Authorization: `Bearer ${getToken()}` }
-        })
-    },
-    deleteAdminTweets ({id}) {
-        return apiHelper.delete(`/admin/tweets/${id}`, {
-            headers: { Authorization: `Bearer ${getToken()}` }
-        })
-    },
+  getAdminTweets() {
+    return apiHelper.get('/admin/tweets', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  deleteAdminTweets({ id }) {
+    return apiHelper.delete(`/admin/tweets/${id}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
 
 }
