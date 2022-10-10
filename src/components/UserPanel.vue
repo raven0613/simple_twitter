c2c4c575402d03ec6f270eebbaedb9cedbe66548
YohaLin <kate1995y@gmail.com>
2022-10-10 19:17:57 +0800

    modify: resolve conflict

--- src/components/UserPanel.vue
+++ src/components/UserPanel.vue
@@ -123,11 +123,16 @@ export default {
       }),
     },
   },
+  data() {
+    return {
+      isEditModalToggled :this.iniIsModalToggled
+    }
+  },
   methods: {
     toggleModal() {
       console.log("123");
-      this.isModalToggled = true;
-      this.$emit("after-toggle-modal", this.isModalToggled);
+      this.isEditModalToggled = true;
+      this.$emit("after-toggle-modal", this.isEditModalToggled);
     },
   },
 };
